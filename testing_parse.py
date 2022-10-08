91559a22067e58f9549a69c78da869fec853fdbe
naren-sre <114977296+naren-sre@users.noreply.github.com>
2022-10-08 13:01:37 -0700

    Update testing_parse.py

--- testing_parse.py
+++ testing_parse.py
@@ -4,15 +4,15 @@ def parsebyword(file,type,word):
     try:
 
         count=0
-        with open(file, "r") as f:  ``` open file in read mode and read each line if type is set then change word and line to lowercase(casefold) and add read occurance by splitting till the word of interest```
+        with open(file, "r") as f:  '''open file in read mode and read each line if type is set then change word and line to lowercase(casefold) and add read occurance into list by splitting till the word of your interest'''
             for line in f:
                 if type==True:
                     word = word.casefold()
                     line = line.casefold()
 
-                linearray = line.split(word)
-                temp = len(linearray) - 1
-                count = count + temp
+                line = line.split(word)
+                temp = len(line) - 1 '''substract -1 from total length of list because after last split of line by word we do not need that value'''
+                count = count + temp  '''use temp variable to track count value through each line in file'''
         return count
 
     except:
@@ -26,10 +26,10 @@ def main():
     parser.add_argument("filename", help="path to a file")
     parser.add_argument("-i", help="input your case insensitve ", action='store_true')
     parser.add_argument("-w", help="input your word", type=str,default='')
-    args = parser.parse_args()
+    args = parser.parse_args() ''' read all arguments into dict'''
 
     count=0
-    word=args.w
+    word=args.w '''user provided args about word of interest will be read into word'''
     f1 = args.filename
     try:
         count = parsebyword(f1,args.i,word)
